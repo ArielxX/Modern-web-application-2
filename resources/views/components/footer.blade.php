dbf74e4edaf2794dfe536e4990988482ad135f60
Ariel Cruz Cruz <acruzx94@gmail.com>
2022-01-17 17:26:35 +0100

    change template

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,24 +1,23 @@
-<footer id="footer" class="bg-gray-800 py-16">
-    <div class="container">
-        {{ $slot }}
-    </div>
-</footer>
-
-<section class="bg-gray-800 py-6 border-t-2 border-gray-700 border-dotted">
-    <div class="container">
-        <div class="flex flex-wrap">
-            <div class="w-full text-center">
-                <p class="text-white">Designed and Developed by <a class="text-white duration-300 hover:text-blue-600"
-                        href="https://tailwindtemplates.co" rel="nofollow">TailwindTemplates</a> and <a
-                        class="text-white duration-300 hover:text-blue-600" href="https://uideck.com"
-                        rel="nofollow">UIdeck</a></p>
+    <!-- footer area -->
+    <div class="w-100 bg-gray-800 text-gray-300 py-12">
+        <div class="block p-5 max-w-6xl mx-auto z-0 min-h-full flex flex-row justify-between">
+            <div>
+                <div class="footer-logo mb-3">
+                    <img src="img/HS-blog-logo.png" width="150" height="40" alt="Logo">
+                </div>
             </div>
+            <div>
+                And some legal mentions
+            </div>
+            <div>
+                And typically some contact info
+            </div>
+
         </div>
     </div>
-</section>
 
-<!-- Go to Top Link -->
-<a href="#"
-    class="back-to-top w-10 h-10 fixed bottom-0 right-0 mb-5 mr-5 flex items-center justify-center rounded-full bg-blue-600 text-white text-lg z-20 duration-300 hover:bg-blue-400">
-    <i class="lni lni-arrow-up"></i>
-</a>
+    <!-- Go to Top Link -->
+    <a href="#"
+        class="back-to-top w-10 h-10 fixed bottom-0 right-0 mb-5 mr-5 flex items-center justify-center rounded-full bg-blue-600 text-white text-lg z-20 duration-300 hover:bg-blue-400">
+        <i class="lni lni-arrow-up"></i>
+    </a>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,68 +1,44 @@
 <header id="header-wrap" class="relative">
-    <!-- Navbar Start -->
-    <div class="navigation fixed top-0 left-0 w-full z-30 duration-300">
-        <div class="container">
-            <nav class="navbar py-2 navbar-expand-lg flex justify-between items-center relative duration-300">
-                <a class="navbar-brand" href="index.html">
-                    <img src="img/HS-blog-logo.png" width="200" height="50" alt="Logo">
-                </a>
-                <button class="navbar-toggler focus:outline-none block lg:hidden" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="toggler-icon"></span>
-                    <span class="toggler-icon"></span>
-                    <span class="toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse hidden lg:block duration-300 shadow absolute top-100 left-0 mt-full bg-white z-20 px-5 py-3 w-full lg:static lg:bg-transparent lg:shadow-none"
-                    id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto justify-center items-center lg:flex">
-                        <li class="nav-item">
-                            <a class="page-scroll active" href="#hero-area">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="page-scroll" href="#services">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="page-scroll" href="#feature">feature</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="page-scroll" href="#team">Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="page-scroll" href="#testimonial">Testimonial</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="page-scroll" href="#pricing">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="page-scroll" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>HS Blog</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+
+
+    <div class="block w-100 bg-white shadow z-50">
+
+        <div class="max-w-6xl mx-auto py-4  text-gray-600 text-sm  flex justify-between">
+            <img src="img/HS-blog-logo.png" width="200" height="50" alt="Logo">
+
+            <div class="px-6 flex flex-row justify-center gap-x-6">
+                <a href="" class="hover:underline decoration-pink-500 decoration-2">Home</a>
+                <a href="" class="hover:underline decoration-pink-500 decoration-2">Posts</a>
+                <a href="" class="hover:underline decoration-pink-500 decoration-2">Authors</a>
+            </div>
+            <div class="px-6">
                 @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/dashboard') }}"
-                                class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log
-                                in</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}"
-                                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                            @endif
-                        @endauth
-                    </div>
+                    @auth
+                        <a href="{{ url('/home') }}" class="hover:underline">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="hover:underline decoration-pink-500 decoration-2">Log in</a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class="ml-4 hover:underline decoration-pink-500 decoration-2">Register</a>
+                        @endif
+                    @endauth
                 @endif
-                <div class="header-btn hidden sm:block sm:absolute sm:right-0 sm:mr-16 lg:static lg:mr-0"
-                    style="margin-right: 120px">
-                    <a class="text-blue-600 border border-blue-600 px-10 py-3 rounded-full duration-300 hover:bg-blue-600 hover:text-white"
-                        href="#feature">FAQ</a>
-                </div>
-            </nav>
+            </div>
         </div>
     </div>
-    <!-- Navbar End -->
 </header>
